459e27b8ff71f8dc1581a327f7fbf95033302b9d
3A417029 <howardzhu0709@outlook.com>
2018-10-20 17:44:53 +0800

    create Lara.php

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,11 @@
+<?php
+    namespace Demo\Hello;
+    
+    /*  Lara 內含有一個function ，
+        function 功能為print 出 Hello Lara ~
+    */
+    class Lara{
+        function __construct(){
+            echo 'Hello Lara~'.PHP_EOL;
+        }
+    }
