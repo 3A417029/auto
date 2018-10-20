f300e80b3281d9cf8eb5159c3b903e58b8ff5179
3A417029 <howardzhu0709@outlook.com>
2018-10-20 17:42:07 +0800

    create vender .gitgnore and HelloWorld.php

---
+++ src/Demo/Hello/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+    namespace Demo;
+    
+    /*  類別HelloWord 內含有一個function ，
+        function 功能為print 出 Hello World ~
+    */
+    class HelloWorld{
+        function __construct(){
+       	    echo 'Hello World ~'.PHP_EOL;
+        }
+    }
